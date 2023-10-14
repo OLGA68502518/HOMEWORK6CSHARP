f7f729ede8ee51c9eea9c31241862792726ac088
TselovalnikovaOlga <olg68502518@yandex.ru>
2023-10-14 22:07:43 +0300

    add gitignore Zadacha041 Zadacha043

---
+++ Zadacha041/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+ 
+        while(true)
+        {
+            Console.Write("Введите число (для выхода введите 'exit'): ");
+            string input = Console.ReadLine();
+ 
+            if(input.ToLower() == "exit")
+            {
+                break;
+            }
+ 
+            int number;
+            if(int.TryParse(input, out number)&& number > 0)
+            {
+                count++;
+            }
+        }
+        
+        Console.WriteLine("Количество введенных чисел больше 0: " + count);
+        
+}
+}
